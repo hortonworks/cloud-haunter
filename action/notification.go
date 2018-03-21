86a947596144ee61fc99f61f694982b2729515bf
Richard Kovacs <kovacsricsi@gmail.com>
2018-03-21 13:14:18 +0100

    Fix HipChat dry run message

--- action/notification.go
+++ action/notification.go
@@ -20,7 +20,7 @@ type NotificationAction struct {
 
 func (a NotificationAction) Execute(instances []*types.Instance) {
 	if len(instances) > 0 {
-		message := instancesMessage{instances: instances}
+		message := instancesMessage{Instances: instances}
 		wg := sync.WaitGroup{}
 		wg.Add(len(ctx.Dispatchers))
 		for n, d := range ctx.Dispatchers {
@@ -37,13 +37,13 @@ func (a NotificationAction) Execute(instances []*types.Instance) {
 }
 
 type instancesMessage struct {
-	instances []*types.Instance
+	Instances []*types.Instance `json:"Instances"`
 }
 
 func (m *instancesMessage) Message() string {
 	var buffer bytes.Buffer
 	buffer.WriteString("/code\n")
-	for _, instance := range m.instances {
+	for _, instance := range m.Instances {
 		buffer.WriteString(fmt.Sprintf("[%s] instance name: %s created: %s\n", instance.CloudType, instance.Name, instance.Created))
 	}
 	return buffer.String()
