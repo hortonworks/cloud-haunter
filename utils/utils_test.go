abf9f1391192f24bcc6186fa3b2ef9750c85475c
Richard Kovacs <kovacsricsi@gmail.com>
2018-07-05 12:06:36 +0200

    More fixing around tests and build

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -18,6 +18,15 @@ type CallInfo struct {
 	invocations []string
 }
 
+func Test_givenValidUnixTimeStamp_whenConvertTimeUnix_thenReturnsConvertedToTime(t *testing.T) {
+
+	var unixTimestamp = "1527244797"
+	convertedTime := ConvertTimeUnix(unixTimestamp)
+
+	assert := assert.New(t)
+	assert.Equal(convertedTime.In(time.UTC), time.Date(2018, 5, 25, 10, 39, 57, 0, time.UTC))
+}
+
 func Test_givenNotAnIntUnixTimeStamp_whenConvertTimeUnix_thenReturnsEpochZeroTime(t *testing.T) {
 
 	var unixTimestamp = "not an int number"
