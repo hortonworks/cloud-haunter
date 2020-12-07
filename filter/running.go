7b579f3137f9ce6cce4558c252ac2ba50100d4c1
David Bajzath <bajzathd@gmail.com>
2020-12-07 09:51:54 +0100

    CB-9986 Fix AWS init and add new running filter types

--- filter/running.go
+++ filter/running.go
@@ -22,6 +22,21 @@ func (f running) Execute(items []types.CloudItem) []types.CloudItem {
 				log.Debugf("[RUNNING] Filter instance, because it's not in RUNNING state: %s", item.GetName())
 				return false
 			}
+		case types.Stack:
+			if item.GetItem().(types.Stack).State != types.Running {
+				log.Debugf("[RUNNING] Filter stack, because it's not in RUNNING state: %s", item.GetName())
+				return false
+			}
+		case types.Database:
+			if item.GetItem().(types.Database).State != types.Running {
+				log.Debugf("[RUNNING] Filter database, because it's not in RUNNING state: %s", item.GetName())
+				return false
+			}
+		case types.Disk:
+			if item.GetItem().(types.Disk).State != types.Unused {
+				log.Debugf("[RUNNING] Filter disk, because it's in used state: %s", item.GetName())
+				return false
+			}
 		default:
 			log.Fatalf("[RUNNING] Filter does not apply for cloud item: %s", item.GetName())
 		}
