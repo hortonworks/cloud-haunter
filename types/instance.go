3529f7e43c1539e9f09ac03ee2a6ebf6d5229472
Brett Lentz <brett.lentz@tamr.com>
2021-07-26 14:00:37 -0400

    add support for doing HTTP request against instances.

--- types/instance.go
+++ types/instance.go
@@ -1,6 +1,15 @@
 package types
 
-import "time"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"io"
+	"net"
+	"net/http"
+	"time"
+)
 
 type InstanceContainer struct {
 	instances []*Instance
@@ -33,6 +42,7 @@ type Instance struct {
 	Metadata     map[string]string `json:"Metadata"`
 	Region       string            `json:"Region"`
 	Ephemeral    bool              `json:"Ephemeral"`
+	IpAddress    string            `json:"IpAddress"`
 }
 
 // Tags Key-value pairs of the tags on the instances
@@ -73,4 +83,76 @@ func (i Instance) GetType() string {
 
 func (i Instance) GetTags() Tags {
 	return i.Tags
+}
+
+type JsonResponseBody struct {
+	Json map[string]interface{}
+}
+
+type RemoteResponse struct {
+	Code  int
+	Body  string
+	Json  JsonResponseBody
+	Error bool
+}
+
+// GetUrl returns the result of an HTTP request to the instance
+func (i Instance) GetUrl(path string, port string) RemoteResponse {
+	var uri string
+	checkPort := "80"
+
+	if port == "" {
+		uri = fmt.Sprintf("http://%s%s", i.IpAddress, path)
+	} else {
+		uri = fmt.Sprintf("http://%s:%s%s", i.IpAddress, port, path)
+		checkPort = port
+	}
+
+	if tcp_port_test(i.IpAddress, checkPort) == false {
+		return RemoteResponse{999, "", JsonResponseBody{}, true}
+	}
+
+	log.Debugf("Making HTTP request to %s", uri)
+	resp, err := http.Get(uri)
+	if err != nil {
+		log.Errorf("Error fetching %s - %s", uri, err)
+		return RemoteResponse{999, "", JsonResponseBody{}, true}
+	}
+	log.Debug("HTTP response: ", resp.Status)
+
+	if resp.ContentLength < 1 {
+		log.Debug("HTTP request returned empty response.")
+		return RemoteResponse{resp.StatusCode, "", JsonResponseBody{}, true}
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Error("Error reading response body", err)
+	}
+
+	if resp.Header.Get("Content-Type") == "application/json" {
+		jsonbody := JsonResponseBody{}
+		json.Unmarshal(body, &jsonbody)
+		log.Debug("HTTP Body: ", jsonbody)
+		return RemoteResponse{resp.StatusCode, "", jsonbody, false}
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	return RemoteResponse{resp.StatusCode, buf.String(), JsonResponseBody{}, false}
+}
+
+func tcp_port_test(host string, port string) bool {
+	timeout := time.Second
+	conn, err := net.DialTimeout("tcp", net.JoinHostPort(host, port), timeout)
+	if err != nil {
+		log.Debugf("Error making TCP connection to %s -> %s", net.JoinHostPort(host, port), err)
+	}
+	if conn != nil {
+		defer conn.Close()
+		log.Debugf("TCP Port %s is open.", net.JoinHostPort(host, port))
+		return true
+	}
+	return false
 }
