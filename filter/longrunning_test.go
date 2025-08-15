1601d907d08297fe13c7de6fa31e8522b0277934
isumegi <isumegi@cloudera.com>
2025-08-15 16:07:00 +0200

    CB-30274: Extend Cloud Haunter's weekly resource deletion with longrunning filter option

--- filter/longrunning_test.go
+++ filter/longrunning_test.go
@@ -34,3 +34,32 @@ func TestLongRunningFilter(t *testing.T) {
 
 	assert.Equal(t, 1, len(filteredItems))
 }
+
+func TestLongRunningFilterOnResources(t *testing.T) {
+	now := time.Now()
+	items := []types.CloudItem{
+		&types.Resource{
+			CloudType:    types.AWS,
+			Name:         "short running load balancer",
+			Created:      now.Add(-defaultRunningPeriod).Add(1 * time.Second),
+			ResourceType: types.LoadBalancer,
+		},
+		&types.Resource{
+			CloudType:    types.AWS,
+			Name:         "long running load balancer",
+			Created:      now.Add(-defaultRunningPeriod).Add(-1 * time.Second),
+			ResourceType: types.LoadBalancer,
+		},
+		&types.Resource{
+			CloudType:    types.AWS,
+			Name:         "VPC",
+			ResourceType: types.Vpc,
+		},
+	}
+
+	filteredItems := longRunning{defaultRunningPeriod}.Execute(items)
+
+	assert.Equal(t, 2, len(filteredItems))
+	assert.NotEqual(t, "short running load balancer", filteredItems[0].GetName())
+	assert.NotEqual(t, "short running load balancer", filteredItems[1].GetName())
+}
