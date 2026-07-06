2c10bfd309f5ca787b1fcbc19a09147b90bf286a
pkergyik <pkergyik@cloudera.com>
2026-07-06 20:37:05 +0200

    CB-28557 Report storage action test

--- action/object_storage_report.go
+++ action/object_storage_report.go
@@ -1,6 +1,10 @@
 package action
 
 import (
+	"encoding/csv"
+	"fmt"
+	"os"
+
 	ctx "github.com/hortonworks/cloud-haunter/context"
 	"github.com/hortonworks/cloud-haunter/types"
 	log "github.com/sirupsen/logrus"
@@ -14,7 +18,16 @@ type StorageReportAction struct {
 }
 
 func (a StorageReportAction) Execute(op types.OpType, filter []types.FilterType, items []types.CloudItem) {
+	file, err := os.Create(fmt.Sprintf("report.csv"))
+	if err != nil {
+		log.Fatalf("Could not create file: %s", err)
+	}
+	defer file.Close()
+	csvWriter := csv.NewWriter(file)
+	defer csvWriter.Flush()
+
 	for _, item := range items {
+		csvWriter.Write([]string{item.GetCloudType().String(), item.GetName()})
 		log.Infof("[%s] %s", item.GetCloudType(), item.GetName())
 	}
 }
