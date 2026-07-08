9c99bb068a06c735fcd981185a59e2b5301a8d4e
pkergyik <pkergyik@cloudera.com>
2026-07-08 15:45:03 +0200

    CB-28557 Timestamp replace colon  with dash

--- action/cloud_items_report.go
+++ action/cloud_items_report.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	ctx "github.com/hortonworks/cloud-haunter/context"
@@ -20,6 +21,7 @@ type CloudItemsReportAction struct {
 
 func (a CloudItemsReportAction) Execute(op types.OpType, filter []types.FilterType, items []types.CloudItem) {
 	isoTimestamp := time.Now().UTC().Format(time.RFC3339)
+	isoTimestamp = strings.ReplaceAll(isoTimestamp, ":", "-") // colon is disallowed on some platforms (e.g. MS/Windows).
 	fileName := fmt.Sprintf("report-cloud-items-%s.csv", isoTimestamp)
 
 	log.Info("Generating cloud items report.")
