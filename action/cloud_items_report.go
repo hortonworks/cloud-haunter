5f9da2c9446cbaf179c2b2f021ae2406f67ad24a
pkergyik <pkergyik@cloudera.com>
2026-07-08 15:56:18 +0200

    CB-28557 Removed extra logging.

--- action/cloud_items_report.go
+++ action/cloud_items_report.go
@@ -37,7 +37,6 @@ func (a CloudItemsReportAction) Execute(op types.OpType, filter []types.FilterTy
 	csvWriter.Write([]string{"Cloud Type", "Name", "Owner"})
 	for _, item := range items {
 		csvWriter.Write([]string{item.GetCloudType().String(), item.GetName(), item.GetOwner()})
-		log.Infof("[%s] %s", item.GetCloudType(), item.GetName())
 	}
 	log.Infof("Written cloud items report to %s", fileName)
 }
